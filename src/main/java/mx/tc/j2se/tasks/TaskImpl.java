21a373a001c73ee26523c8f28bdb992da18a23ef
raulgaar <raulgaar16@gmail.com>
2022-06-19 13:01:14 -0500

    First practice

--- src/main/java/mx/tc/j2se/tasks/TaskImpl.java
+++ src/main/java/mx/tc/j2se/tasks/TaskImpl.java
@@ -1,16 +1,39 @@
+/*
+ * TaskImpl
+ *
+ * 1
+ *
+ * Raul Arturo Garcia Aranda
+ *
+ */
 package mx.tc.j2se.tasks;
-
+/*
+*
+* This is an implementation of the Task interface
+*
+*
+* */
 public class TaskImpl implements Task{
 
-    private String title;
-    private int time, start, end, interval;
-    private boolean active, repeated;
+    private String title;           // Title of the task
+    private int time;               // Time of execution of the task
+    private int start;              // Start of execution of the task
+    private int end;                // End of execution of the task
+    private int interval;           // Interval of execution of the task
+    private boolean active;         //Status of the task
+    private boolean repeated;       //Status of repeatability of the task
 
 
-
+    //Creates a new Task
     public TaskImpl() {
 
     }
+
+    /*
+    * Creates a new Task with the given title and time
+    * @param title
+    * @param time
+    *  */
     public TaskImpl(String title, int time) {
         active = false;
         repeated = false;
@@ -18,6 +41,13 @@ public class TaskImpl implements Task{
         this.time = time;
     }
 
+    /*
+    * Creates a new Task with the given title, start end and interval
+    * @param title
+    * @param start
+    * @param end
+    * @param interval
+    * */
     public TaskImpl(String title, int start, int end, int interval) {
         active = false;
         repeated = true;
@@ -27,107 +57,163 @@ public class TaskImpl implements Task{
         this.interval = interval;
     }
 
+    /*
+    * Gets the title of the task
+    * @return title
+    * */
     @Override
     public String getTitle() {
         return title;
     }
 
+    /*
+    * Sets the title of the task
+    * @param title
+    * */
     @Override
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /*
+    * Gets the status of the task
+    * @return active
+    * */
     @Override
     public boolean isActive() {
         return active;
     }
 
+    /*
+    * Sets the status of the task
+    * @param active
+    * */
     @Override
     public void setActive(boolean active) {
         this.active = active;
     }
 
+    /*
+    * Gets the time of execution of the task
+    * @return time
+    * */
     @Override
     public int getTime() {
-        int t;
+        int time;                          // Time of the task
         boolean r = isRepeated();
         if (r) {
-            t = this.start;
+            time = this.start;
         }else {
-            t = this.time;
+            time = this.time;
         }
-        return t;
+        return time;
     }
 
+    /*
+    * Sets the time of execution for non-repetitive tasks
+    * @param time
+    * */
     @Override
     public void setTime(int time) {
         this.time = time;
         repeated = false;
     }
 
+    /*
+    * Gets the start time of a repetitive task
+    * @return startTime
+    * */
     @Override
     public int getStartTime() {
-        int t;
+        int startTime;                     // Start time of the task
 
-        if (this.interval != 1) {
-            t = this.start;
+        if (this.interval != 0) {
+            startTime = this.start;
         }else {
-            t = this.time;
+            startTime = this.time;
         }
-        return t;
+        return startTime;
     }
 
+    /*
+     * Gets the start time of a repetitive task
+     * @return endTime
+     * */
     @Override
     public int getEndTime() {
-        return this.end;
+        int endTime;                       //End time of the task
+
+        if (this.interval != 0) {
+            endTime = this.end;
+        }else {
+            endTime = this.time;
+        }
+        return endTime;
     }
 
+    /*
+    * Gets the interval of repetitive tasks
+    * @return interval
+    * */
     @Override
     public int getRepeatInterval() {
-        int i;
-        boolean r = isRepeated();
-        if (r) {
-            i = this.interval;
-            setTime(this.start,this.end, i);
-        }else {
-            i = 0;
-            setTime(this.time);
-        }
-        return i;
+        return this.interval;
     }
 
+    /*
+    * Set the start time, end time and interval of the repetitive tasks
+    * @param start
+    * @param end
+    * @param interval
+    * */
     @Override
     public void setTime(int start, int end, int interval) {
         this.start = start;
         this.end = end;
         this.interval = interval;
-        boolean r = isRepeated();
-        if (!r) {
+        if (!isRepeated()) {
             this.repeated = true;
         }
     }
 
+    /*
+    * Gets the repeatability of the tasks
+    * @return repeated
+    * */
     @Override
     public boolean isRepeated() {
-        int interval = this.interval;
-        if(interval == 1){
+        int interval = this.interval;       //Local variable for the interval value
+        if(interval == 0){
             repeated = false;
-        }else if(interval > 1) {
+        }else if(interval > 0) {
             repeated = true;
         }else{
-            System.out.println("Interval must be greater than zero for repetitive tasks or 0 for non-repetitive tasks");
+            System.out.println("Interval must be greater than zero for" +
+                    " repetitive tasks or 0 for non-repetitive tasks");
         }
         return repeated;
     }
 
+    /*
+    * Gets the next time of start time of a task given a time
+    * @param current
+    * */
     @Override
     public int nextTimeAfter(int current) {
-        int time = getTime();
-        boolean active = isActive();
-        if(active) {
-            if (current <= time) {
-                return time;
-            }
+        int startTime = getTime();         //Local variable for the start time of the task
+        int endTime = getEndTime();        //Local variable for the end time of the task
+        if(!isActive()) {
+        return -1;
+        }
+        if (current < startTime) {
+            return startTime;
+        }else if(current >= startTime && current < endTime) {
+            int elapsed = current - startTime;
+            int intervalIndex = (elapsed/this.interval) + 1;
+            int next = (this.interval * intervalIndex);
+            return next;
+        }else if(current >= startTime && current == endTime) {
+            return current;
         }
         return -1;
     }
